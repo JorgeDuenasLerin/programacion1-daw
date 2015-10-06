4e7a11eafd80998598c9942bce72ce16d644bc6c
alumno <alumno@iesavellaneda.org>
2015-10-06 13:13:34 +0200

    Ejemplo del paraguasX

--- ejemplos-evaluacion-1/proyecto-bluej/MeLlevoParaguas.java
+++ ejemplos-evaluacion-1/proyecto-bluej/MeLlevoParaguas.java
@@ -7,21 +7,23 @@
  */
 public class MeLlevoParaguas
 {
-  public static void main( String args[] ){
-  
-      // DATOS, LOS CAMBIO PARA PROBAR CASOS
-      boolean llueveHoy = true;
-      boolean llovioAyer = true;
-      boolean llovioAnteayer = true;
-      
-      // CUENTAS CON LOS DATOS, ESTO NO CAMBIA CUANDO CAMBIEN LOS DATOS
-      // usando !, &&, ||, (, )
-      bolean llevo paraguas = ....;
-      bolean llevo chubasquero = ....;
-      
-      // MIRO LO QUE SALE
-      System.out.println( "paraguas:" + paraguas );
-      System.out.println( "chubasquero:" + chubasquero );
-     
-  }
+    public static void main( String args[] ){
+
+        // DATOS, LOS CAMBIO PARA PROBAR CASOS
+        boolean llueveHoy = true;
+        boolean llovioAyer = false;
+        boolean llovioAnteayer = true;
+
+        // CUENTAS CON LOS DATOS, ESTO NO CAMBIA CUANDO CAMBIEN LOS DATOS
+        // usando !, &&, ||, (, )
+        boolean chubasquero = llovioAyer && llovioAnteayer;
+        boolean paraguas = (llueveHoy || llovioAyer) && !chubasquero;
+
+        // MIRO LO QUE SALE
+        System.out.println( "___________________________" );
+        System.out.println( "hoy:" + llueveHoy + "  ayer:" + llovioAyer + "  anteayer:" + llovioAnteayer );
+        System.out.println( "paraguas:" + paraguas );
+        System.out.println( "chubasquero:" + chubasquero );
+
+    }
 }
