9544f5fc9432e94ea2a7c080172973c67aae2bf7
unknown <agonzalez@t1serv>
2016-02-12 14:12:04 +0100

    Calendario con bisiestos

--- ejemplos-evaluacion-2/src/Calendario.java
+++ ejemplos-evaluacion-2/src/Calendario.java
@@ -3,37 +3,87 @@ public class Calendario {
 
 	private int dia;
 	private int mes;
+	private int año;
 	
 	public Calendario(){
-		dia = 1;
-		mes = 1;
+		this(1,1,2016);
+	}
+	
+	public Calendario(int dia, int mes, int año){
+		this.dia = dia;
+		this.mes = mes;
+		this.año = año;
 	}
 	
 	public void avanzaDia(){
 		dia += 1;
-		if( dia > diasDemes(mes) ){
+		if( dia > diasDeMes(mes,año) ){
 			dia = 1;
 			mes +=1;
 		}
 		if( mes > 12 ){
 			mes = 1;
+			año += 1;
 		}
 	}
 	
-	private int diasDemes(int mes) {
-		int meses[] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
-		return meses[mes];
+	public void avanzaSemana(){
+		for( int i = 0 ; i < 7 ; i++ ){
+			avanzaDia();
+		}
+	}
+	
+	public void avanzaMes(){
+		mes += 1;
+		if( mes > 12 ){
+			año += 1;
+			mes = 1;
+		}
+		normalizaDiaDentroDelMes();
+	}
+
+	private void normalizaDiaDentroDelMes() {
+		int diasDeMes = diasDeMes(mes, año);
+		if( dia > diasDeMes ){
+			dia = diasDeMes;
+		}
+	}
+	
+	public void avanzaAño(){
+		año += 1;
+		normalizaDiaDentroDelMes();
+	}
+	
+	private static int diasDeMes(int mes, int año) {
+		int mesesNormales[]  = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+		int mesesBisiestos[] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+		if( esBisiesto(año) )
+			return mesesBisiestos[mes];
+		else
+			return mesesNormales[mes];
+	}
+
+	private static boolean esBisiesto(int año) {
+		if( año % 400 == 0 ){
+			return true;
+		}
+		if( año % 100 == 0 ){
+			return false;
+		}
+		return año % 4 == 0;
 	}
 
 	@Override
 	public String toString() {
-		return String.format( "%02d/%02d", dia,mes );
+		return String.format( "%02d/%02d/%d", dia,mes, año );
 	}
 	
+	
 	public static void main(String[] args) {
-		Calendario c = new Calendario();
-		for( int i = 0 ; i < 366*2 ; c.avanzaDia(), i++ ){
-			System.out.println(c);
-		}
+		Calendario c = new Calendario(31,1,2000);
+		c.avanzaMes();
+		System.out.println(c);
+		c.avanzaAño();
+		System.out.println(c);
 	}
 }
